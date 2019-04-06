0f0bfc85d8f65b76a4d27e1ce494e5477a67c5e3
Felipe Oliveira Gutierrez <felipe.o.gutierrez@gmail.com>
2019-04-06 13:29:54 +0200

    added cities

--- src/main/java/org/sense/util/Cities.java
+++ src/main/java/org/sense/util/Cities.java
@@ -3,7 +3,9 @@ package org.sense.util;
 public enum Cities {
 	PARIS("Paris"), ROME("Rome"), LONDON("London"), BERLIN("Berlin"), BARCELONA("Barcelona"), AMSTERDAM("Amsterdam"),
 	StPETERSBURG("St. Petersburg"), ISTANBUL("Istanbul"), ATHENS("Athens"), COPENHAGEN("Copenhagen"), MADRID("Madrid"),
-	BRUSSELS("Brussels"), BUDAPEST("Budapest"), MUNICH("Munich"), EDINBURGH("Edinburgh"), PRAGUE("Prague");
+	BRUSSELS("Brussels"), BUDAPEST("Budapest"), MUNICH("Munich"), EDINBURGH("Edinburgh"), PRAGUE("Prague"),
+	MILAN("Milan"), VIENNA("Vienna"), LISBON("Lisbon"), STOCKHOLM("Stockholm"), DUBLIN("Dublin"), MOSCOW("Moscow"),
+	OSLO("Oslo"), FLORENCE("Florence"), OXFORD("Oxford"), CANNES("Cannes"), HELSINKI("Helsinki"), BRUGES("Bruges");
 
 	private String value;
 
